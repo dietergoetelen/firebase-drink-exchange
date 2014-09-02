07f7d53588a5dfc4b6ba61f1b0c89bd2e35d1aa8
Dieter Goetelen <dieter.goetelen@hotmail.com>
2014-09-02 11:05:36 +0200

    vote function

--- public/app/exchange/controllers/exchangeCtrl.js
+++ public/app/exchange/controllers/exchangeCtrl.js
@@ -3,6 +3,15 @@
     
     var ExchangeCtrl = (function () {
         
+        var calculatePrice = {
+            '+': function (price, rating) {
+                return +(price + (rating / 100)).toFixed(2);
+            },
+            '-': function (price, rating) {
+                return +(price - (rating / 100)).toFixed(2);
+            }
+        };
+        
         function ExchangeCtrl(scope, DataService) {
             this.scope = scope;
             this.dataService = DataService;
@@ -12,14 +21,33 @@
         
         ExchangeCtrl.prototype.bindExchange = function () {
             var vm = this;
-            
             var syncObject = vm.dataService.get('').$asObject();
             
             syncObject.$bindTo(vm.scope, 'overview');
         };
         
         ExchangeCtrl.prototype.vote = function (exchange) {
-            console.log(exchange);
+            var updatedExchange = exchange,
+                vm = this;
+            
+            updatedExchange.amount = updatedExchange.amount + 1;
+            updatedExchange.price = calculatePrice['+'](updatedExchange.price, updatedExchange.rating);
+            
+            // Update this price
+            vm.dataService.set(exchange.name, updatedExchange);
+            
+            // Update other prices
+            angular.forEach(vm.scope.overview, function (ex) {
+                if (ex && ex.name != exchange.name) {
+                    ex.price = calculatePrice['-'](ex.price, ex.rating);
+                    
+                    if (ex.price < 1) {
+                        ex.price = 1;
+                    }
+                    
+                    vm.dataService.set(ex.name, ex);
+                }
+            });
         };
 
         ExchangeCtrl.$inject = ['$scope', 'DataService'];
